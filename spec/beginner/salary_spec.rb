a66dea98d8b6f7e5420ba5f1e94137b80552ddaf
Willian <william.silva@Vindi.com.br>
2023-12-13 15:45:20 -0300

    Ajustes Rubocop

--- spec/beginner/salary_spec.rb
+++ spec/beginner/salary_spec.rb
@@ -1,20 +1,24 @@
+# beginner/salary_spec.rb
+
+# frozen_string_literal: true
+
 require 'salary'
 
 RSpec.describe Salary do
   describe '#calculate_salary' do
     it 'calculate salary correctly' do
       salary = Salary.new
-      expect { salary.calculate_total_salary("Willian", 500.00, 1230.30) }.to output("TOTAL = R$ 684.54\n").to_stdout
+      expect { salary.calculate_total_salary('Willian', 500.00, 1230.30) }.to output("TOTAL = R$ 684.54\n").to_stdout
     end
 
     it 'calculates salary correctly with zero salary' do
       salary = Salary.new
-      expect { salary.calculate_total_salary("John", 0.00, 500.00) }.to output("TOTAL = R$ 75.00\n").to_stdout
+      expect { salary.calculate_total_salary('John', 0.00, 500.00) }.to output("TOTAL = R$ 75.00\n").to_stdout
     end
-    
+
     it 'calculates salary correctly with zero sold in cash' do
       salary = Salary.new
-      expect { salary.calculate_total_salary("Alice", 1000.00, 0.00) }.to output("TOTAL = R$ 1000.00\n").to_stdout
+      expect { salary.calculate_total_salary('Alice', 1000.00, 0.00) }.to output("TOTAL = R$ 1000.00\n").to_stdout
     end
   end
 end
